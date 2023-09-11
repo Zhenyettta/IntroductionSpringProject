de9328b022e3fa34e1c2cfdfa09f8efd379ae291
Anisimov_Eugene <zhecky2004@gmail.com>
2023-09-11 11:01:14 +0300

    Initial commit

---
+++ src/main/java/com/naukma/introductionspringproject/IntroductionSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.naukma.introductionspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionSpringProjectApplication.class, args);
+	}
+
+}
