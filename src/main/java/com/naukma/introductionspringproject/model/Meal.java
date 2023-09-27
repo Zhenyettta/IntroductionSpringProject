b48b8ee45c01aa976ef8255d23bca6e746c01ced
Zhenyettta <zhecky2004@gmail.com>
2023-09-27 13:04:09 +0300

    fixed application properties

--- src/main/java/com/naukma/introductionspringproject/model/Meal.java
+++ src/main/java/com/naukma/introductionspringproject/model/Meal.java
@@ -1,11 +1,25 @@
 package com.naukma.introductionspringproject.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.*;
+import jakarta.persistence.*;
 
 @Getter
 @Setter
 @ToString
 @RequiredArgsConstructor
 public class Meal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double price;
+
+
 
 }
