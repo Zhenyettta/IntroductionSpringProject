03b70612528d49346300caec75886e8c12f8b375
Dashap03 <plakhotnaia@gmail.com>
2023-12-14 23:57:41 +0200

    UI

--- server/src/main/java/com/naukma/introductionspringproject/controller/LoginController.java
+++ server/src/main/java/com/naukma/introductionspringproject/controller/LoginController.java
@@ -5,12 +5,15 @@ import com.naukma.introductionspringproject.entity.MealEntity;
 import com.naukma.introductionspringproject.service.MealService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 import java.util.List;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:3000", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}, allowCredentials = "true", allowedHeaders = "*")
 public class LoginController {
     private final MealService mealService;
 
